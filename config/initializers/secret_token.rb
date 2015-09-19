0de4afbf1bc8f57c03af814f5b58e6b83b31cff5
Eugene Khashin <adastreamer@gmail.com>
2015-09-19 19:52:24 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemandRails::Application.config.secret_token = '6d1a3dc8111d4d16bf74a2a09994f5688fe29418feb3a9f3fa2e9cebf1ccee83aadd4cb08ec2ad60d576124b50ebf7b32790a92ff022dfb26ab2b85864de97b1'
